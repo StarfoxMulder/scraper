4375d6be7d0c9b7f9b8979e0611d0fe09f32053a
Charles Prestia <cjprestia@gmail.com>
2016-12-11 20:08:41 -0500

    pushing more polished version -- still having trouble getting the res from mongodb display in handlebars on this branch...

--- public/app.js
+++ public/app.js
@@ -11,9 +11,29 @@
 //// After talking with intended users, this is what they want */
 $.getJSON("/vigilantcitizen", function(data) {
   for (var i = 0; i < data.length; i++) {
-    $("#articles").append("<p data-id='"+data[i]._id+"'><a href='"+data[i].link+"'>"+data[i].title+"</a></p>")
+    $("#articles").append("<div class='media'><div class='media-left'><img class='media-object' src='"+data[i].image+"'></div><div class='media-body'><a href='"+data[i].link+"'><h4 class='media-heading'>"+data[i].title+"</h4></a>"+data[i].source+"</div><div class='media-right'><button type='button' class='btn btn-info btn-lg' data-id='"+data[i]._id+"'><span class='glyphicon glyphicon-comment' aria-hidden='true'></span>Comments</button></div></div>")
   }
 });
+
+
+/*
+$("#articles").append("<p data-id='"+data[i]._id+"'><a href='"+data[i].link+"'>"+data[i].title+"</a>"+data[i].source+"</p>")
+*/
+
+/*
+$("#articles").append("<div class='media'><div class='media-left'><img class='media-object' src='"+data[i].image+"'></div>
+  <div class='media-body'>
+    <a href='"+data[i].link+"'>
+      h4 class='media-heading'>"+data[i].title+"</h4>
+    </a>"+data[i].source+"
+  </div>
+  <div class='media-right'>
+    <button type='button' class='btn btn-info btn-lg' data-id='"+data[i]._id+"'>
+      <span class='glyphicon glyphicon-comment' aria-hidden='true'></span>Comments
+    </button>
+  </div>
+</div>
+/*
 
 // // Above Top Secret ajax
 // $.getJSON("/abovetopsecret", function(data) {
