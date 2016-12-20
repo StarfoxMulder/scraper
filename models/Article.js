a5fc1135dcc32a527fa09f60a19dad2ca58e2375
Charles Prestia <cjprestia@gmail.com>
2016-12-20 18:23:49 -0500

    pushing with Article again

---
+++ models/Article.js
@@ -0,0 +1,38 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// Creating schema
+var ArticleSchema = new Schema({
+
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: false
+  },
+  source: {
+    type: String,
+    required: true
+  },
+  scrapeDate: {
+    type: Date,
+    default: Date.now
+  },
+  notes: [{
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }]
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
